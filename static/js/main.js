2d79d3dccbcf22654f4f8942f345fbec59a217dc
annakertesz <kerteszannanak@gmail.com>
2016-09-27 14:51:23 +0200

    NewBoard is finally working

--- static/js/main.js
+++ static/js/main.js
@@ -1,36 +1,36 @@
-function Board(title, content){
-    this.title = title;
-    this.content = content;
-    this.balloons = [];
+// function Board(title, content){
+//     this.title = title;
+//     this.content = content;
+//     this.balloons = [];
+//
+//     this.addBalloon = function (balloon) {
+//         this.balloons.push(balloon);
+//     };
+// };
+//
+// function Balloon(title, content, owner){
+//     this.title = title;
+//     this.content = content;
+//     owner.addBalloon(this);
+// };
+//
+// var first_board = new Board('Shopping to do', 'These are the things you should buy')
+// var milk = new Balloon('Milk', 'Lactose free delicious milk', first_board)
+// var bread = new Balloon('Bread', 'Some crusty French bread straight from Clermont-Ferrand', first_board)
+//
+// localStorage.setItem('first', JSON.stringify(first_board));
+//
+// var retrievedObject = localStorage.getItem('first');
+// console.log('retrievedObject: ', JSON.parse(retrievedObject));
 
-    this.addBalloon = function (balloon) {
-        this.balloons.push(balloon);
-    };
-};
 
-function Balloon(title, content, owner){
-    this.title = title;
-    this.content = content;
-    owner.addBalloon(this);
-};
 
-var first_board = new Board('Shopping to do', 'These are the things you should buy')
-var milk = new Balloon('Milk', 'Lactose free delicious milk', first_board)
-var bread = new Balloon('Bread', 'Some crusty French bread straight from Clermont-Ferrand', first_board)
 
-localStorage.setItem('first', JSON.stringify(first_board));
 
-var retrievedObject = localStorage.getItem('first');
-console.log('retrievedObject: ', JSON.parse(retrievedObject));
-
-$(document).ready(function() {
-    $('button').click(function() {
-    	var toAdd = $("input[name=message]").val();
-        $('#messages').append("<p>"+toAdd+"</p>");
-    });
-});
 
 $('#create_new_board').click(function(){
-    $('#big_board').append("<div id='board'></div>");
+    var title = $("input[name=title]").val();
+    $('#big_board').append("<div id='board'><p id='title'>"+title+"</p></div>");
+    console.log(title);
 
 });
