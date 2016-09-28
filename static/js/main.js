083d5dc6a9694ac7aac3d4c20e3644410cf6f64a
handriss <andrashinkel@gmail.com>
2016-09-28 17:22:17 +0200

    2nd validation remark resolved

--- static/js/main.js
+++ static/js/main.js
@@ -7,39 +7,25 @@ function Board(title){
     };
 };
 
+
 function Card(title, content, owner){
     this.title = title;
     this.content = content;
     owner.addCard(this);
 };
-//
-// var first_board = new Board('Shopping to do', 'These are the things you should buy')
-// var milk = new Balloon('Milk', 'Lactose free delicious milk', first_board)
-// var bread = new Balloon('Bread', 'Some crusty French bread straight from Clermont-Ferrand', first_board)
-//
-// localStorage.setItem('first', JSON.stringify(first_board));
-//
-// var retrievedObject = localStorage.getItem('first');
-// console.log('retrievedObject: ', JSON.parse(retrievedObject));
 
 
 $(document).ready(function () {
-    if (typeof localStorage.getItem("boards") !== 'undefined'){
-        var boards = JSON.parse(localStorage.getItem("boards"));
-    }else{
+    if(localStorage.getItem("boards") == null){
         var boards = new Array();
+    }else{
+        var boards = JSON.parse(localStorage.getItem("boards"));
     }
 
-    localStorage.setItem("boards", JSON.stringify(boards));
-    // console.log(typeof JSON.parse(localStorage.getItem("boards")));
-
-    // console.log(localStorage.getItem('boards'));
-//     for ( var i = 0, len = localStorage.length; i < len; ++i ) {
-//         var title = JSON.parse(localStorage.getItem( localStorage.key( i ) ));
-//         $('#big_board').append("<div id='board'><p id='title'>"+title["title"]+"</p></div>");
-// }
+    for(var i = 0; i < boards.length; i++){
+        $('#big_board').append("<div id='board'><p id='title'>" + boards[i].title +"</p></div>");
+    }
 });
-
 
 
 $('#create_new_board').click(function(){
@@ -47,7 +33,10 @@ $('#create_new_board').click(function(){
     $('#big_board').append("<div id='board'><p id='title'>"+title+"</p></div>");
     $("input[name=title]").val('');
 
-    var mylist = JSON.parse(localStorage.getItem("boards"));
-    console.log(mylist);
-
+    var boards = JSON.parse(localStorage.getItem("boards"));
+    if(boards == null){
+        boards = new Array();
+    }
+    boards.push(new Board(title));
+    localStorage.setItem("boards", JSON.stringify(boards));
 });
