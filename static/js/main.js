b33138669567a3aeaae094f52fac554dfa38abfb
handriss <andrashinkel@gmail.com>
2016-09-28 17:36:23 +0200

    saveBoard and getBoard functions added

--- static/js/main.js
+++ static/js/main.js
@@ -16,18 +16,13 @@ function Card(title, content, owner){
 
 
 $(document).ready(function () {
-    if(localStorage.getItem("boards") == null){
-        var boards = new Array();
-    }else{
-        var boards = JSON.parse(localStorage.getItem("boards"));
-    }
-
-    for(var i = 0; i < boards.length; i++){
-        $('#big_board').append("<div id='board'><p id='title'>" + boards[i].title +"</p></div>");
-    }
+    drawBoards();
 });
 
 
+/**
+ * Creates a new Board instance with the name of the content of the inputbox of name title.
+ */
 $('#create_new_board').click(function(){
     var title = $("input[name=title]").val();
     $('#big_board').append("<div id='board'><p id='title'>"+title+"</p></div>");
@@ -37,6 +32,44 @@ $('#create_new_board').click(function(){
     if(boards == null){
         boards = new Array();
     }
-    boards.push(new Board(title));
-    localStorage.setItem("boards", JSON.stringify(boards));
 });
+
+
+/**
+ * Returns a Board object whose title is equal to the given in title.
+ * If there is no Board instance with the given name, return null.
+ */
+function getBoard(title){
+    boards = JSON.parse(localStorage.getItem("boards"));
+    for(var i = 0; i < boards.length; i++){
+        if(boards[i].title == title){
+            return boards[i];
+        }
+    }
+    return null;
+}
+
+/**
+ * Handles saving a new Board object into the localStorage.
+ */
+function saveBoard(board){
+    boards = JSON.parse(localStorage.getItem("boards"));
+    boards.push(board);
+    localStorage.setItem("boards", JSON.stringify(boards));
+}
+
+
+/**
+ * Handles drawing existing Board items from the localStorage.
+ */
+function drawBoards(){
+    if(localStorage.getItem("boards") == null){
+        var boards = new Array();
+    }else{
+        var boards = JSON.parse(localStorage.getItem("boards"));
+    }
+
+    for(var i = 0; i < boards.length; i++){
+        $('#big_board').append("<div id='board'><p id='title'>" + boards[i].title +"</p></div>");
+    }
+}
